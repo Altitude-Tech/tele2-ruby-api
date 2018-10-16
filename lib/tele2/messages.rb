1fe693a1826ea62b93b92cfad156c0a9dccc8d4e
Pedro <w0pedro0w@gmail.com>
2018-10-16 16:34:24 +0100

    add .

--- lib/tele2/messages.rb
+++ lib/tele2/messages.rb
@@ -10,9 +10,42 @@ module Tele2
       self.client = client
     end
 
-    def get_message(smsId)
-      response = self.client.get_request("/smsMessages/#{smsId.to_s}")
+    def get_message(smsMsgId)
+      response = self.client.get_request("/smsMessages/#{smsMsgId.to_s}")
       return Message.new(self.client, response)
+    end
+
+    def find_by_accound_id(account_id)
+      response = self.client.get_request("/smsMessages?accountId=#{account_id.to_s}")
+
+      messages = Array.new
+      response.each do |message|
+        messages << get_message(message)
+      end
+
+      return messages
+    end
+
+    def find_from_date(from_date)
+      response = self.client.get_request("/smsMessages?fromDate=#{CGI.escape(from_date.iso8601.to_s)}")
+
+      messages = Array.new
+      response.each do |message|
+        messages << get_message(message)
+      end
+
+      return messages
+    end
+
+    def find_by_time_period(from_date, to_date)
+      response = self.client.get_request("/smsMessages?fromDate=#{CGI.escape(from_date.iso8601.to_s)}&toDate=#{CGI.escape(to_date.iso8601.to_s)}")
+
+      messages = Array.new
+      response.each do |message|
+        messages << get_message(message)
+      end
+
+      return messages
     end
 
   end
