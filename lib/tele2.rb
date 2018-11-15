8460dc11ec11343f4efcf59f2a555e2c2e553617
megan-jones <megan.jones@altered-carbon.com>
2018-11-15 16:07:30 +0000

    update

--- lib/tele2.rb
+++ lib/tele2.rb
@@ -12,18 +12,18 @@ require_relative 'tele2/errors'
 #tele2 = Tele2::Devices.new('Frazerbarnes', '9a13a30b-d8d8-4e01-8908-04eefcf1a9e7')
 #puts(tele2.find_by_accound_id(231121205, Time.new(2018))['devices'])
 
-#client = Tele2::Base.new('meganjones', '92cc67f7-8f28-473d-8246-baf469c02542')
+# client = Tele2::Base.new('meganjones', 'c7a8b1c0-c20e-43e3-a5a9-759209dd0e3b')
 # client = Tele2::Base.new('Frazerbarnes', '9a13a30b-d8d8-4e01-8908-04eefcf1a9e7')
-#
+ 
 # devices = client.devices.find_by_account_id(231121205, Time.new(2018))
 # puts(devices)
-#
+
 # devices.each do |d|
 #   iccid = d.iccid
 #   puts(iccid)
 #   # usage = d.usage
 #   session = d.session
-#
+
 # end
 
 # zone_usage = client.devices.get_zone_usage(iccid)
