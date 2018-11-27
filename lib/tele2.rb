3e1aa599f8d74dca87c61237bbea9f6bb546d62d
megan-jones <megan.jones@altered-carbon.com>
2018-11-27 12:02:58 +0000

    update

--- lib/tele2.rb
+++ lib/tele2.rb
@@ -13,24 +13,24 @@ require_relative 'tele2/errors'
 #puts(tele2.find_by_accound_id(231121205, Time.new(2018))['devices'])
 
 # client = Tele2::Base.new('meganjones', 'c7a8b1c0-c20e-43e3-a5a9-759209dd0e3b')
-client = Tele2::Base.new('Frazerbarnes', '9a13a30b-d8d8-4e01-8908-04eefcf1a9e7')
+# client = Tele2::Base.new('Frazerbarnes', '9a13a30b-d8d8-4e01-8908-04eefcf1a9e7')
 
-devices = client.devices.find_by_account_id(231121205, Time.new(2018))
-puts(devices)
+# devices = client.devices.find_by_account_id(231121205, Time.new(2018))
+# puts(devices)
 
 
 # zone_usages = Array.new
-devices.each do |d|
- iccid = d.iccid
- puts(iccid)
- history = client.devices.get_audit_history(iccid)
- puts(history)
+# devices.each do |d|
+#  iccid = d.iccid
+#  puts(iccid)
+#  history = client.devices.get_audit_history(iccid)
+#  puts(history)
  # zone_usages << client.devices.get_zone_usage(iccid)
  # puts(zone_usages)
  # usage = d.usage.ctd_data_usage
  # puts(d.usage)
  # session = d.session
-end
+# end
 
 # zu = client.devices.get_zone_usage(89462036051001589081)
 # zone = zu.zone
